b936500afc1f487b6ad846da39df5a48587712d3
Laughing0826 <155951133+Laughing0826@users.noreply.github.com>
2024-01-11 15:42:56 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.showLeds(`
+    . . # # .
+    . # . . #
+    . # . . .
+    . # . . #
+    . . # # .
+    `)
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.pause(500)
+    }
+})
